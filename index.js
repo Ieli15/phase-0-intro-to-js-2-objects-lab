18cfca29f6f5200a1a168ab0288f75c03fa9b20a
Elias Bosire Ayunga <bosire.elias@gmail.com>
2024-12-06 05:24:13 +0300

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+// Employee Object
+const employee = {
+    name: "Elias",
+    streetAddress: "44 Banda"
+  };
+  
+  // Functions
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
